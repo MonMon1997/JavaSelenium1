aa117f4ce31120d84bf9343fce72e676738e3280
MonMon1997 <monmonmonmonmon11112@gmail.com>
2025-01-04 05:12:07 +0800

    l

--- src/test/java/GoogleWalkthrough/googleSearch_properties.java
+++ src/test/java/GoogleWalkthrough/googleSearch_properties.java
@@ -1,13 +1,12 @@
 package GoogleWalkthrough;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class googleSearch_properties {
-    public static void main(String[] args) throws IOException {
-        setProperties();
+    public static void main(String[] args) {
         getProperties();
-
     }
 
     public static void getProperties() {
@@ -16,19 +15,11 @@ public class googleSearch_properties {
          InputStream stream = new FileInputStream("C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_2\\src\\test\\java\\GoogleWalkthrough\\config.properties");
          properties.load(stream);
          String browser = properties.getProperty("browser");
-         String path = properties.getProperty("path");
-
-         System.out.println(browser + "\n"+ path);
+         System.out.println(browser);
+         System.out.println();
       } catch (Exception e){
           e.printStackTrace();
       }
     }
 
-    public static void setProperties() throws IOException {
-        Properties properties = new Properties();
-        OutputStream outputStream = new FileOutputStream("C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_2\\src\\test\\java\\GoogleWalkthrough\\config.properties");
-        properties.setProperty("browser", "Chrome");
-        properties.setProperty("path", "C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_2\\chromedriver.exe");
-        properties.store(outputStream,null);
-    }
 }
