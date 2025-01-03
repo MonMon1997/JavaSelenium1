3218cfcc14f62978e09cc53dd1837ec2efba3c57
MonMon1997 <monmonmonmonmon11112@gmail.com>
2025-01-04 03:36:28 +0800

    seven commit, excel file added.

--- src/test/java/GoogleWalkthrough/googleSearch_properties.java
+++ src/test/java/GoogleWalkthrough/googleSearch_properties.java
@@ -1,25 +1,28 @@
 package GoogleWalkthrough;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 public class googleSearch_properties {
-    public static void main(String[] args) {
-        getProperties();
+    public static void main(String[] args) throws IOException {
+        setProp();
+        getProp();
     }
 
-    public static void getProperties() {
-      try{
-         Properties properties = new Properties();
-         InputStream stream = new FileInputStream("C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_2\\src\\test\\java\\GoogleWalkthrough\\config.properties");
-         properties.load(stream);
-         String browser = properties.getProperty("browser");
-         System.out.println(browser);
-         System.out.println();
-      } catch (Exception e){
-          e.printStackTrace();
-      }
+    public static void getProp() throws IOException {
+        Properties properties = new Properties();
+        InputStream stream = new FileInputStream("C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_2\\src\\test\\java\\GoogleWalkthrough\\config.properties");
+        properties.load(stream);
+        String browser = properties.getProperty("browser");
+        String path = properties.getProperty("path");
+        System.out.println(browser + "\n" + path);
     }
 
+    public static void setProp() throws IOException{
+        Properties properties = new Properties();
+        OutputStream stream = new FileOutputStream("C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_2\\src\\test\\java\\GoogleWalkthrough\\config.properties");
+        properties.setProperty("browser", "Edge");
+        properties.setProperty("path", "C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_2\\chromedriver.exe");
+        properties.store(stream,null);
+    }
 }
